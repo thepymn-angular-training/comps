6ba3cdabef93bb71411099e6f5b72cffd0b3f025
Peyman <usestate.pk@gmail.com>
2021-04-29 16:15:34 +0430

    added item list component

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -10,6 +10,18 @@ export class ViewsHomeComponent implements OnInit {
     { value: 22, label: '# Of Users' },
     { value: 900, label: 'Total Stars' },
     { value: 367, label: 'Reviews' },
+  ];
+  items = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a good couch to sit on'
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dersser',
+      description: 'This is a good dresser to put things on'
+    }
   ]
 
   constructor() { }
